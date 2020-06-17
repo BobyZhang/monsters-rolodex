3a7e383c4a8782b83ccfbe05455c01cf94d4b4f6
BobyZhang <zsb0213@gmail.com>
2020-06-17 11:02:40 +1000

    Updates

---
+++ precache-manifest.ab3714bf80f419da1374b87e0348c5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86da0584abe56f3a3c6e75ae3afd1b82",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3a796f49d5f1333d9e0d",
+    "url": "/monsters-rolodex/static/css/main.ff950df0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a796f49d5f1333d9e0d",
+    "url": "/monsters-rolodex/static/js/main.9e93315a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
